aba80c2ea058b748b2e4ac42427511329b5a6e18
St Pardon <okc4pardon@gmail.com>
2023-05-19 18:57:02 +0100

    :new: added more svg

--- server/public/index.js
+++ server/public/index.js
@@ -1,10 +1,29 @@
 const custom = document.getElementById('custom');
 const phrase = document.querySelector('.phrase');
+const url = document.querySelector('.s_url');
+const copy = document.querySelector('.copy');
 
-custom.onchange = (e) => {
-  if (e.target.checked) {
-    phrase.style.display = 'block';
-  } else {
-    phrase.style.display = 'none';
-  }
-};
+if (custom) {
+  custom.onchange = (e) => {
+    if (e.target.checked) {
+      phrase.style.display = 'block';
+    } else {
+      phrase.style.display = 'none';
+    }
+  };
+}
+
+if (copy) {
+  copy.onclick = (e) => {
+    navigator.clipboard.writeText(url.innerText);
+
+    copy.innerHTML = 'copied';
+    copy.style.color = 'green';
+    copy.style.fontSize = '1rem';
+
+    setTimeout(() => {
+      copy.innerHTML =
+        '<img src="assets/copy-svgrepo-com.svg" alt="copy" title="copy" class="icon">';
+    }, 3000);
+  };
+}
