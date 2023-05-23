110b407c8489443cb7c0cd836b6b6f512a350877
St Pardon <okc4pardon@gmail.com>
2023-05-23 19:07:03 +0100

    :bug: fixed typescript error for shortner route

--- src/routes/shortener.route.ts
+++ src/routes/shortener.route.ts
@@ -41,6 +41,7 @@ Shortener.get(
     '',
     async (req: Request & { user?: any }, res: Response): Promise<void> => {
       const { original_url, phrase } = req.body;
+      let urlExist;
 
       if (!original_url) {
         res.status(400).json({ err: 'Please provide a url' });
@@ -58,20 +59,41 @@ Shortener.get(
       );
 
       if (req.user?.email) {
-        const urlExist = await urlModel.findOne({
+        urlExist = await urlModel.findOne({
           original_url,
           user_id: req.user.email,
         });
-
-        if (!urlExist) {
-          await urlModel.create({
-            user_id: req.user.email,
-            original_url,
-            shortened_url: `http://127.0.0.1:5353/${shorten_code}`,
-            qrcode,
-          });
-        }
       }
+
+      if (urlExist){
+        res.status(201).render('index', {
+          url: {
+            res: {
+              original_url: urlExist.original_url,
+              shortened_url: urlExist.shortened_url,
+              qrcode: urlExist.qrcode,
+            },
+            user: req.user
+              ? {
+                  email: req.user?.email,
+                  name: `${req.user?.first_name} ${req.user?.last_name}`,
+                  dp: req.user?.photo,
+                }
+              : '',
+          },
+        });
+        return;
+      }
+
+      // if (req.user?.email && !urlExist) {
+        await urlModel.create({
+          user_id: req.user ? req.user.email : '',
+          original_url,
+          shortened_url: `http://127.0.0.1:5353/${shorten_code}`,
+          qrcode,
+        });
+      // }
+
       res.status(201).render('index', {
         url: {
           res: {
