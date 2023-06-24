2a5647577b0c92eb51e56aa34103dd156cb4ee58
St Pardon <okc4pardon@gmail.com>
2023-06-24 13:04:49 +0100

    :wrench: completed url tests

--- tests/url.spec.ts
+++ tests/url.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../src/app';
 import User from '../src/models/user.model';
 import { connect } from './db-config';
+import redisClient from '../src/config/redis.config';
 
 describe('Test url route', () => {
   let connectDB: any;
@@ -9,6 +10,7 @@ describe('Test url route', () => {
   beforeAll(async (): Promise<void> => {
     console.log('Starting the server...');
     connectDB = await connect();
+    redisClient.connect(); // connect to redis
   });
 
   afterEach(async () => {
@@ -17,6 +19,7 @@ describe('Test url route', () => {
 
   afterAll(async () => {
     await connectDB.disconnect();
+    await redisClient.disconnect();
   });
 
   it('should shortern a url for an unauthenicated user', async () => {
@@ -37,6 +40,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('');
+    expect(res.body.clicks).toBe(0);
   });
 
   it('should shortern a url without a sub domain for an unauthenicated user', async () => {
@@ -57,6 +61,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('');
+    expect(res.body.clicks).toBe(0);
   });
 
   it("shouldn't shortern an invalid url for an unauthenicated user", async () => {
@@ -108,6 +113,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('john@doe.com');
+    expect(res.body.clicks).toBe(0);
   });
 
   it('should shortern a url without a sub domain for an authenicated user', async () => {
@@ -144,6 +150,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('john@doe.com');
+    expect(res.body.clicks).toBe(0);
   });
 
   it("shouldn't shortern an invalid url for an authenicated user", async () => {
@@ -196,6 +203,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('');
+    expect(res.body.clicks).toBe(0);
     expect(res.body.shortened_url.split('/')[3]).toBe('google');
   });
 
@@ -218,6 +226,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('');
+    expect(res.body.clicks).toBe(0);
     expect(res.body.shortened_url.split('/')[3]).toBe('google');
   });
 
@@ -272,6 +281,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('john@doe.com');
+    expect(res.body.clicks).toBe(0);
     expect(res.body.shortened_url.split('/')[3]).toBe('seminar');
   });
 
@@ -310,6 +320,7 @@ describe('Test url route', () => {
     expect(res.body).toHaveProperty('created_at');
     expect(res.body).toHaveProperty('updated_at');
     expect(res.body.user_id).toBe('john@doe.com');
+    expect(res.body.clicks).toBe(0);
     expect(res.body.shortened_url.split('/')[3]).toBe('seminar');
   });
 
@@ -326,5 +337,174 @@ describe('Test url route', () => {
       .set('content-type', 'application/json');
 
     expect(res.status).toBe(302);
+  });
+
+  it('should get url info by id for an authenticated user', async () => {
+    await User.create({
+      first_name: 'John',
+      last_name: 'doe',
+      email: 'john@doe.com',
+      password: '12345678',
+    });
+
+    const user = await request(app)
+      .post('/api/v1/auth/signin')
+      .set('content-type', 'application/json')
+      .send({
+        email: 'john@doe.com',
+        password: '12345678',
+      });
+
+    const url = await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.google.com',
+      });
+
+    const res = await request(app)
+      .get(`/api/v1/url/${url.body._id}`)
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('user_id');
+    expect(res.body).toHaveProperty('original_url');
+    expect(res.body).toHaveProperty('shortened_url');
+    expect(res.body).toHaveProperty('qrcode');
+    expect(res.body).toHaveProperty('clicks');
+    expect(res.body).toHaveProperty('_id');
+    expect(res.body).toHaveProperty('created_at');
+    expect(res.body).toHaveProperty('updated_at');
+    expect(res.body.user_id).toBe('john@doe.com');
+    expect(res.body.clicks).toBe(0);
+    expect(res.body._id).toBe(url.body._id);
+  });
+
+  it("shouldn't get url info by id for an unauthenticated user", async () => {
+    await User.create({
+      first_name: 'John',
+      last_name: 'doe',
+      email: 'john@doe.com',
+      password: '12345678',
+    });
+
+    const user = await request(app)
+      .post('/api/v1/auth/signin')
+      .set('content-type', 'application/json')
+      .send({
+        email: 'john@doe.com',
+        password: '12345678',
+      });
+
+    const url = await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.google.com',
+      });
+
+    const res = await request(app)
+      .get(`/api/v1/url/${url.body._id}`)
+      .set('content-type', 'application/json');
+
+    expect(res.status).toBe(401);
+    expect(res.text).toBe('Unauthorized');
+  });
+
+  it('should get all url info for an authenticated user', async () => {
+    await User.create({
+      first_name: 'John',
+      last_name: 'doe',
+      email: 'john@doe.com',
+      password: '12345678',
+    });
+
+    const user = await request(app)
+      .post('/api/v1/auth/signin')
+      .set('content-type', 'application/json')
+      .send({
+        email: 'john@doe.com',
+        password: '12345678',
+      });
+
+    await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.google.com',
+      });
+    await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.facebook.com',
+      });
+    await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.twitter.com',
+      });
+
+    const res = await request(app)
+      .get(`/api/v1/url/history`)
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body.length).toEqual(3);
+  });
+
+  it("shouldn't get all url info for an authenticated user", async () => {
+    await User.create({
+      first_name: 'John',
+      last_name: 'doe',
+      email: 'john@doe.com',
+      password: '12345678',
+    });
+
+    const user = await request(app)
+      .post('/api/v1/auth/signin')
+      .set('content-type', 'application/json')
+      .send({
+        email: 'john@doe.com',
+        password: '12345678',
+      });
+
+    await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.google.com',
+      });
+    await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.facebook.com',
+      });
+    await request(app)
+      .post('/api/v1/url')
+      .set('content-type', 'application/json')
+      .set('Authorization', 'Bearer ' + user.body.token)
+      .send({
+        original_url: 'https://www.twitter.com',
+      });
+
+    const res = await request(app)
+      .get(`/api/v1/url/history`)
+      .set('content-type', 'application/json');
+
+    expect(res.status).toBe(401);
+    expect(res.text).toBe('Unauthorized');
   });
 });
